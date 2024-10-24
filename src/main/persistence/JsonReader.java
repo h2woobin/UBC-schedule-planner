0b8ece30aea84acba2cb9329a020ec0c776c120f
Woobin Kim <hikevin@student.ubc.ca>
2024-10-24 16:51:30 -0700

    Fix reader and writer class

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,65 @@
+package persistence;
+
+import model.Exam;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.List;
+
+import org.json.*;
+
+// Represents a reader that reads workroom from JSON data stored in file
+public class JsonReader {
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
+
+    // EFFECTS: reads workroom from file and returns it;
+    // throws IOException if an error occurs reading data from file
+    public Exam read() throws IOException {
+        String jsonData = readFile(source);  
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseExam(jsonObject);
+    }
+
+    // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
+    }
+
+    // EFFECTS: parses Exam from JSON object and returns it
+    private Exam parseExam(JSONObject jsonObject) {
+        String subject = jsonObject.getString("Subject");
+        int time = jsonObject.getInt("Time");
+        int date = jsonObject.getInt("Date");
+        String location = jsonObject.getString("Location");
+        int goalMark = jsonObject.getInt("Goal Mark");
+        
+        Exam ex = new Exam(subject, date, time, location, goalMark);
+        return ex;
+    }
+
+    // MODIFIES: wr
+    // EFFECTS: parses thingies from JSON object and adds them to workroom
+    private void addExams(List<Exam> examList, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("exams"); /////
+        for (Object json : jsonArray) {
+            JSONObject nextExamJson = (JSONObject) json;
+            Exam exam = parseExam(nextExamJson);
+            examList.add(exam);
+        }
+    }
+
+}
