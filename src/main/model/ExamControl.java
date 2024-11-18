a079b4aa7da76706e06a64916a35f70f07ece737
Woobin Kim <hikevin@student.ubc.ca>
2024-11-17 16:13:30 -0800

    Fixed ExamControal method

--- src/main/model/ExamControl.java
+++ src/main/model/ExamControl.java
@@ -2,53 +2,80 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-// Handle all exam lists that user made.
+
+import persistence.Writable;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ExamControl {
+public class ExamControl implements Writable {
+    private List<Exam> examList = new ArrayList<>();
+    private Exam examObject;
 
-    public class InnerExamControl {
-        private List<Exam> examList = new ArrayList<>();
 
-        public List<Exam> getExamList() {
-            return examList;
+    public void addSubject(String subject, int date, int time, String location, int goalMark) {
+        examObject = new Exam(subject, date, time, location, goalMark);
+
+        if (examList.stream().noneMatch(e -> e.getSub().equals(subject))) {
+            examList.add(examObject);
         }
-
-        /*
-         * REQUIRES: subject should be not null String.
-         * MODIFIES: this.examList
-         * EFFECTS: remove all imformation what user wants to remove. Check if the
-         * subject that user wants to delete
-         * is in the exam list or not. If it's in it, it will be removed and return
-         * true. Otherwise, mothod will give false.
-         */
-        public boolean removeExam(String subject) {
-            for (int i = 0; i < examList.size(); i++) {
-                Exam exam = examList.get(i);
-                if (exam.getSub().equalsIgnoreCase(subject)) {
-                    examList.remove(i);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public void addExam(Exam exam) {
-            examList.add(exam);
-        }
-
-        public JSONObject toJson() {
-            JSONArray jsonArray = new JSONArray();
-            for (Exam exam : examList) {
-                jsonArray.put(exam.toJson());
-            }
-
-            JSONObject json = new JSONObject();
-            json.put("exams", jsonArray);
-            return json;
-        }
-
     }
+
+    public void deleteSubject(String subject) {
+        for (Exam e : examList) {
+            if (e.getSub().equals(subject)) {
+                examList.remove(e);
+            }
+        }
+    }
+
+    public void addActualMark(String subject, int actMark) {
+        for (Exam e : examList) {
+            if (e.getSub().equals(subject)) {
+                e.setActMark(actMark);
+            }
+        }
+    }
+
+    public double averageScore() {
+        double total = 0;
+        for (Exam e : examList) {
+            total += e.getActMark();
+        }
+        return total / examList.size();
+    }
+
+    public String gpa() {
+        String gpa = "";
+        double average = averageScore();
+        if (average >= 90) {
+            gpa = "A+";
+        } else if (average >= 80) {
+            gpa = "B+";
+        } else if (average >= 70) {
+            gpa = "C+";
+        } else if (average >= 60) {
+            gpa = "D";
+        } else {
+            gpa = "F";
+        }
+        return gpa;
+    }
+
+    public List<Exam> getExamList() {
+        return examList;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONArray jsonArray = new JSONArray();
+        for (Exam exam : examList) {
+            jsonArray.put(exam.toJson());
+        }
+
+        JSONObject json = new JSONObject();
+        json.put("exams", jsonArray);
+        return json;
+    }
+
 }
